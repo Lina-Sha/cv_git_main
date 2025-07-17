cfe16183e6dd189da1caa945b1efd08c25743666
Lina-Sha <angelinaweekly15@gmail.com>
2025-07-17 14:24:43 +0300

    Сайт

---
+++ js.js
@@ -0,0 +1,7 @@
+// Объявление переменных
+const button = document.querySelector('button');
+
+/* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
+button.addEventListener('click', function() {
+	alert('Смысл не найти, а создать')
+}) 
